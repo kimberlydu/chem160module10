9d123d7c99e6659d090926011d2f59904bce3d79
kimberlydu <54691917+kimberlydu@users.noreply.github.com>
2019-10-20 22:16:09 -0700

    Add files via upload

---
+++ methane.R
@@ -0,0 +1,18 @@
+V=.25
+n=1.0
+Vbar=V/n
+T=273.
+R=0.083145
+#Add 2 lines here
+P=R*T/Vbar
+cat("Ideal gas law ",P,"\n")
+a=2.3026
+b=0.043067
+#Add 1 line here
+P=R*T/(Vbar-b)-a/Vbar^2 
+cat("Van der Waals equation of state ",P,"\n")
+A=32.205
+B=0.029850
+#Add 1 line here
+P=R*T/(Vbar-B)-A/(sqrt(T)*Vbar*(Vbar+B)) 
+cat("Redlich-Kwong equation of state ",P,"\n")
